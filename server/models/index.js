efb71e6515cca3ad71a7e503ea3e6b3ff550e6ca
Ross F <rfahan@gmail.com>
2023-09-08 13:05:19 -0600

    added comment to index.js model

--- server/models/index.js
+++ server/models/index.js
@@ -2,4 +2,4 @@ const User = require('./User');
 const Post = require('./Post');
 const Forum = require('./Forum');
 const Comment = require('./Comment');
-module.exports = { User, Post, Forum };
+module.exports = { User, Post, Forum, Comment };

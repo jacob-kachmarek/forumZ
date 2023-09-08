2d6f92d5e495c1268e907ef317a59de54a822068
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-09-08 14:46:30 -0700

    adding typedefs/resolvers

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -3,37 +3,40 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const commentSchema = new Schema({
-  text: {
-    type: String,
-    required: true,
-  },
-  createdBy: {
-    type: Schema.Types.ObjectId,
-    ref: 'User', 
-  },
-  replies: [{
-    text: String,
-    createdBy: { 
-        type: Schema.Types.ObjectId,
-        ref: 'User', 
-    }, 
-    createdAt: { 
-        type: Date, 
-        default: Date.now 
+    text: {
+        type: String,
+        required: true,
     },
-    likes: { 
-    type: Number,
-    default: 0
-    }
-  }],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  likes: {
-    type: Number,
-    default: 0,
-  },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    replies: [{
+        text: {
+            type: String,
+            required: true
+        },
+        createdBy: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        likes: {
+            type: Number,
+            default: 0
+        }
+    }],
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    likes: {
+        type: Number,
+        default: 0,
+    },
 });
 
 const Comment = mongoose.model('Comment', commentSchema);

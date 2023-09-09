5451a258796d3a41c1ff893f3c3bddf265ba4187
Thomas Munzar <tmunzar92@gmail.com>
2023-09-09 13:47:34 -0700

    worikng on navbar component

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+
+
+export default function Navbar () {
+    return(
+        <>
+        <h1>NAVBAR</h1>
+        </>
+    )
+        
+    
+}

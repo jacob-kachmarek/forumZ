57e6decaa78e060eb3f1c32488e7c82a6dbd59ea
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-09-09 13:00:39 -0700

    adding components and pages working on basic front end layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,44 @@
-import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { Outlet } from 'react-router-dom';
+
+import Header from './components/Header';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <ApolloProvider client={client}>
+      <div className="flex-column justify-flex-start min-100-vh">
+        <Header />
+        <div className="container">
+          <Outlet />
+        </div>
+      </div>
+    </ApolloProvider>
+  );
 }
 
-export default App
+export default App;

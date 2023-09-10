92425185f19a4a9eabd9c160fdb5539026360de5
Jordan Lopez <jordanlopezemail@gmail.com>
2023-09-09 18:12:23 -0700

    working on styling the nav

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import { Link, useLocation } from 'react-router-dom';
 import Auth from '../../utils/auth';
 import './navbar.css';
 
-const loggedIn = true; // delete this eventually
+const loggedIn = false; // delete this eventually
 
 export default function Navbar () {
    const currentPage = useLocation().pathname;
@@ -14,18 +14,21 @@ export default function Navbar () {
 
     return(
       <>
-         {loggedIn ? (        // change to Auth.loggedIn() eventually;
-            <div>
-               {(currentPage === "/") ? (<Link className='activeLink' to="/" > Home </Link>) : (<Link className='inactiveLink' to="/" > Home </Link>)}
-               <button onClick={handleLogout}> Logout </button>
-            </div>
-         ) : (
-            <div>
-               {(currentPage === "/") ? (<Link className='activeLink' to="/" > Home </Link>) : (<Link className='inactiveLink' to="/" > Home </Link>)}
-               {(currentPage === "/login") ? (<Link className='activeLink' to="/login" > Login </Link>) : (<Link className='inactiveLink' to="/login" > Login </Link>)}
-               {(currentPage === "/signup") ? (<Link className='activeLink' to="/signup" > Signup </Link>) : (<Link className='inactiveLink' to="/signup" > Signup </Link>)}
-            </div>
-         )}
+         <div>
+            {loggedIn ? (        // change to Auth.loggedIn() eventually;
+               <div>
+                  {(currentPage === "/") ? (<Link className='activeLink' to="/" > Home </Link>) : (<Link className='inactiveLink' to="/" > Home </Link>)}
+                  <button onClick={handleLogout}> Logout </button>
+               </div>
+            ) : (
+               <div>
+                  {(currentPage === "/") ? (<Link className='activeLink' to="/" > Home </Link>) : (<Link className='inactiveLink' to="/" > Home </Link>)}
+                  {(currentPage === "/login") ? (<Link className='activeLink' to="/login" > Login </Link>) : (<Link className='inactiveLink' to="/login" > Login </Link>)}
+                  {(currentPage === "/signup") ? (<Link className='activeLink' to="/signup" > Signup </Link>) : (<Link className='inactiveLink' to="/signup" > Signup </Link>)}
+               </div>
+            )}
+
+         </div>
       </>
     );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import './Home.css';
+import '../utils/auth';
+
+const loggedIn = false; // change later // change to Auth.loggedIn() eventually;
+
 export default function Home() {
+    
+    if (!loggedIn){
+        return (
+            <h1>Not logged in home</h1>
+        );
+    }
+    
     return (
         <>
-            <h1>Home</h1>
+            <h1>Logged In home</h1>
         </>
     )
 }
+
+
+// NOT logged In 
+    // render all forums 
+    // search for one
+
+// Logged IN 
+    // render all forums by User
+    // allow creation of a new forum 

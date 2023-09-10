58a4ed85ba4b72e559a065ec31ca8340d0674e06
Jordan Lopez <jordanlopezemail@gmail.com>
2023-09-10 11:26:19 -0700

    updated home css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,6 +34,7 @@ function App() {
         <Navbar />
         <Outlet />
     </ApolloProvider>
+    
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,48 +1,77 @@
-import './Home.css';
+
 import '../utils/auth';
 import { Link } from 'react-router-dom';
 
 const loggedIn = false; // change later // change to Auth.loggedIn() eventually;
 
+const cardStyle = {
+    border: '3px solid #000000',
+    marginBottom: '20px',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '10px',
+    borderRadius: '5px',
+    backgroundColor: '#f0f0f0',
+};
+
+const titleStyle = {
+    fontSize: '16px',
+    fontWeight: 'bold',
+    marginBottom: '5px',
+};
+
+const descriptionStyle = {
+    fontSize: '14px',
+    color: '#333333',
+};
+
+const leftPanelStyle = {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '20px',
+    width: '200px',
+    color: '#000000',
+};
+
 export default function Home() {
     return (
         <>
-            <div className='leftPanel'>
+            <div style={leftPanelStyle}>
                 <div>
-                    <div className='card'>
-                        <div className='title'>Title</div>
-                        <div className='description'>Description goes here for every forum</div>
+                    <div style={cardStyle}>
+                        <div style={titleStyle}>Title</div>
+                        <div style={descriptionStyle}>Description goes here for every forum</div>
                     </div>
                 </div>
                 <div>
-                    <div className='card'>
-                        <div className='title'>Title</div>
-                        <div className='description'>Description goes here for every forum</div>
+                    <div style={cardStyle}>
+                        <div style={titleStyle}>Title</div>
+                        <div style={descriptionStyle}>Description goes here for every forum</div>
                     </div>
                 </div>
                 <div>
-                    <div className='card'>
-                        <div className='title'>Title</div>
-                        <div className='description'>Description goes here for every forum</div>
+                    <div style={cardStyle}>
+                        <div style={titleStyle}>Title</div>
+                        <div style={descriptionStyle}>Description goes here for every forum</div>
                     </div>
                 </div>
                 <div>
-                    <div className='card'>
-                        <div className='title'>Title</div>
-                        <div className='description'>Description goes here for every forum</div>
+                    <div style={cardStyle}>
+                        <div style={titleStyle}>Title</div>
+                        <div style={descriptionStyle}>Description goes here for every forum</div>
                     </div>
                 </div>
                 <div>
-                    <div className='card'>
-                        <div className='title'>Title</div>
-                        <div className='description'>Description goes here for every forum</div>
+                    <div style={cardStyle}>
+                        <div style={titleStyle}>Title</div>
+                        <div style={descriptionStyle}>Description goes here for every forum</div>
                     </div>
                 </div>
             </div>
         </>
     );
 }
-
 
 // NOT logged In 
     // render all forums in left side menu with searchbar

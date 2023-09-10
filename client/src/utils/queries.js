1904effa69193f8577254b1603b73ab9a9018af6
Ross F <rfahan@gmail.com>
2023-09-09 18:46:24 -0600

    fixed a variable name discrepncy error

---
+++ client/src/utils/queries.js
@@ -0,0 +1,83 @@
+import {gql} from '@apollo/client';
+
+export const GET_USER_FORUMS = gql`
+query getSingleUser($username: String!) {
+    getSingleUser(username: $username) {
+      username
+      forums {
+        title
+        description
+        createdAt
+        createdBy {
+          username
+        }
+        posts {
+          title
+          likes
+          image
+          description
+          createdBy {
+            username
+          }
+          comments {
+            text
+            likes
+            createdAt
+            createdBy {
+              username
+            }
+            replies {
+              text
+              likes
+              createdAt
+              createdBy {
+                username
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_ALL = gql`
+query getUsers {
+    getUsers {
+      username
+      forums {
+        title
+        description
+        createdAt
+        createdBy {
+          username
+        }
+        posts {
+          title
+          description
+          likes
+          image
+          createdBy {
+            username
+          }
+          comments {
+            text
+            likes
+            createdAt
+            createdBy {
+              username
+            }
+            replies {
+              text
+              likes
+              createdAt
+              createdBy {
+                username
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`

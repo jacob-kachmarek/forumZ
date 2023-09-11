c65b7920fc1e4ac078080f201e92a696bf9683b3
Jordan Lopez <jordanlopezemail@gmail.com>
2023-09-10 18:56:21 -0700

    working on modals for front end

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import ForumList from '../components/ForumList';
 import '../utils/auth';
 import './Home.css';
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 
 const loggedIn = true;
 
 export default function Home() {
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     if (!loggedIn){
         return (
@@ -31,7 +39,7 @@ export default function Home() {
         );
     }
 
-    else{
+    else{       //logged In = true
         return (
             <div className='containerMain'>
               <div className='leftPanel'>
@@ -41,6 +49,43 @@ export default function Home() {
                 <div style={{width: '50%'}}>
                     {/* <h1 className='heading'>Join The Conversation</h1>  welcome back User.getProfile */}
                     <p className='para'>Your forums:</p>
+                    <Button variant="primary" onClick={handleShow}>
+        Launch demo modal
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
                     {/* Profile Component HERE // not created yet // just a component with all their forums */}
                 </div>
               </div>

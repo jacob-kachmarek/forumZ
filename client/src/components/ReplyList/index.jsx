f768e6b3cfb1897eacfffc7c0b7101939c77744d
Thomas Munzar <tmunzar92@gmail.com>
2023-09-10 22:30:42 -0700

    still not rendering

--- client/src/components/ReplyList/index.jsx
+++ client/src/components/ReplyList/index.jsx
@@ -1,9 +1,13 @@
 import { useQuery } from "@apollo/client";
 import { GET_REPLIES } from "../../utils/queries";
+import { useParams } from 'react-router-dom';
 
-const ReplyList = ({ commentId }) => {
+const ReplyList = () => {
+
+    // forumId, postId,  not sure if i need this with commentId
+    const { commentId } = useParams();
     const { loading, error, data } = useQuery(GET_REPLIES, {
-        variables: { commentId },
+        variables: { commentId: commentId },
     });
 
     if (loading) return <p>Loading...</p>;
@@ -16,6 +20,7 @@ const ReplyList = ({ commentId }) => {
 
     return (
         <div>
+            <h3>Replies</h3>
             {replies.map((reply) => (
                 <div key={reply._id}>
                     <p>{reply.text}</p>
--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -1,7 +1,10 @@
 import CommentList from "../components/CommentList/index.jsx"
 import ReplyList from "../components/ReplyList/index.jsx"
 
+
+
 export default function Post() {
+    const commentId = useParams();
     return (
         <>
             <h1>Post</h1>

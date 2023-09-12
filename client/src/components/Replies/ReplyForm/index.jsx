7b90cff6c313386c09f8861b4f48d2b38862f664
Jacob Kachmarek <jacob.kachmarek@gmail.com>
2023-09-12 16:44:08 -0700

    not working

--- client/src/components/Replies/ReplyForm/index.jsx
+++ client/src/components/Replies/ReplyForm/index.jsx
@@ -1,13 +1,24 @@
 import { ADD_REPLY } from "../../../utils/mutations";
+import { GET_COMMENTS} from "../../../utils/queries"
 import { useState } from "react";
-import { useMutation } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import Auth from '../../../utils/auth';
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
-export default function ReplyForm({commentId}) {
-    const { postId } = useParams();
+export default function ReplyForm() {
+    // const { postId } = useParams();
+    const { loading, error, data } = useQuery(GET_COMMENTS, {
+        variables: { commentId: commentId },
+        
+    });
     const [text, setText] = useState('');
-    const [addReply, { error }] = useMutation(ADD_REPLY);
+    const [addReply] = useMutation(ADD_REPLY);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+    const commentId = data.comment._id;
+    console.log(commentId);
+
     const handleChange = (e) => {
         const { name, value } = e.target;
         if (name === 'text') {
@@ -18,22 +29,22 @@ export default function ReplyForm({commentId}) {
 
         event.preventDefault();
         console.log("commentID", commentId)
-        console.log("POST ID",postId)
+        // console.log("POST ID",postId)
         console.log("USER ID", Auth.getProfile().data._id)
+
         try {
             const { data } = await addReply({
                 variables: {
                     text,
-                    postId: postId,
-                    commentId: commentId,
+                    commentId,
                     userID: Auth.getProfile().data._id,
                 },
             });
-            console.log(data, text, Auth.getProfile().data._id );
+            console.log("data: ", data, text, Auth.getProfile().data._id );
         } catch (err) {
             console.log(err);
         } 
-        window.location.reload();   
+        // window.location.reload();   
         setText("");
     }
     return (

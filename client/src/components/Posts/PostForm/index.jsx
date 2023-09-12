f1388e305a2669274e54aabf2c775bfb7e654df9
Ross F <rfahan@gmail.com>
2023-09-12 13:23:31 -0600

    added functionality to addPost with image

--- client/src/components/Posts/PostForm/index.jsx
+++ client/src/components/Posts/PostForm/index.jsx
@@ -1,8 +1,84 @@
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_POST } from '../../../utils/mutations';
+import Auth from '../../../utils/auth';
+import { Cloudinary } from '@cloudinary/url-gen';
+import { Image } from 'cloudinary-react';
 
-export default function PostForm() {
-    return (
-        <div>
-           <h1>PostForm</h1>
-        </div>
-    )
+export default function PostForm({ forumId }) {
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [image, setImage] = useState(null);
+  const [imageLoading, setImageLoading] = useState(false);
+  const [addPost, { error }] = useMutation(ADD_POST);
+
+  // Initialize cloudinary object
+  const cloudinary = new Cloudinary({
+    cloud: {
+      cloudName: 'dtvso2zih'
+    }
+  });
+
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'forumZupload');
+
+    setImageLoading(true);
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dtvso2zih/image/upload', {
+      method: 'POST',
+      body: formData
+    });
+
+    const data = await res.json();
+    setImage(data.secure_url);
+    setImageLoading(false);
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    const { data } = await addPost({
+      variables: {
+        title,
+        description,
+        image,
+        userID: Auth.getProfile().data._id,
+        forumId,
+      },
+    });
+
+    // Reset form
+    setTitle('');
+    setDescription('');
+    setImage(null);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmit}>
+        <label>Title:</label>
+        <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
+
+        <label>Description:</label>
+        <textarea value={description} onChange={(e) => setDescription(e.target.value)}></textarea>
+
+        <label>Image:</label>
+        <input type="file" onChange={handleImageUpload} />
+        
+        {/* Image Preview */}
+        { imageLoading ? (
+          <p>Loading...</p>
+        ) : image ? (
+          <Image cloudName="forumZupload" publicId={image} width="300" crop="scale" />
+        ) : null }
+
+        <button type="submit">Add Post</button>
+      </form>
+
+      { error && <p>{error.message}</p> }
+    </div>
+  );
 }
--- client/src/components/Posts/PostList/index.jsx
+++ client/src/components/Posts/PostList/index.jsx
@@ -3,43 +3,42 @@ import { useParams } from 'react-router-dom';
 import { GET_FORUM_POSTS } from '../../../utils/queries';
 import { Link } from 'react-router-dom';
 
-
 function PostList() {
-    const { forumId } = useParams();
-    console.log("forumId:", forumId);
+  const { forumId } = useParams();
+  console.log("forumId:", forumId);
 
-    const { loading, error, data } = useQuery(GET_FORUM_POSTS, {
-        variables: { forumId },
-    });
+  const { loading, error, data } = useQuery(GET_FORUM_POSTS, {
+      variables: { forumId },
+  });
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error: {error.message}</p>;
-    console.log("data:", data);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+  console.log("data:", data);
 
-    return (
-        <>
-            <div>
-                <h1>Posts in this Forum</h1>
-                {data && data.getPostsByForum && data.getPostsByForum[0].posts.length === 0 ? (
-                    <h2>This forum does not contain any posts yet...</h2>
-                ) : (
-                    data.getPostsByForum[0].posts.map(post => (
-                        <div key={post._id}>
-                            <Link to={`/forum/${forumId}/post/${post._id}`}>
-                                <h2>{post.title}</h2>
-                            </Link>
-                            <p>{post.description}</p>
-                            <p>By: {post.createdBy.username}</p>
-                            <p>Likes: {post.likes}</p>
-                            <p>Posted on: {post.createdAt}</p>
-                            <p>Image: {post.image}</p>
-                        </div>
-                    ))
-                )}
+  return (
+    <>
+      <div>
+        <h1>Posts in this Forum</h1>
+        {data && data.getPostsByForum && data.getPostsByForum[0].posts.length === 0 ? (
+          <h2>This forum does not contain any posts yet...</h2>
+        ) : (
+          data.getPostsByForum[0].posts.map(post => (
+            <div key={post._id}>
+              <Link to={`/forum/${forumId}/post/${post._id}`}>
+                <h2>{post.title}</h2>
+              </Link>
+              <p>{post.description}</p>
+              <p>By: {post.createdBy.username}</p>
+              <p>Likes: {post.likes}</p>
+              <p>Posted on: {post.createdAt}</p>
+              {/* Check if the image field exists before trying to render it */}
+              {post.image && <img src={`${post.image}?format=auto`} alt={post.title} />}
             </div>
-        </>
-    );
-
+          ))
+        )}
+      </div>
+    </>
+  );
 }
 
 export default PostList;
